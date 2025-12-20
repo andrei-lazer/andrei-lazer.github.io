cab9bff3bf3b7ed335bba2af244602ce0b299314
andrei-lazer <andrei.lazer319@gmail.com>
2025-12-20 10:40:04 +0000

    fixed theme not loading on first visit

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,19 +1,24 @@
- const toggleButton = document.getElementById("theme-toggle");
- const stylesheet = document.getElementById("theme-stylesheet");
- 
- const LIGHT = "/src/style/light.css";
- const DARK = "/src/style/dark.css";
- 
+const toggleButton = document.getElementById("theme-toggle");
+const stylesheet = document.getElementById("theme-stylesheet");
+
+const LIGHT = "/src/style/light.css";
+const DARK = "/src/style/dark.css";
+
 // Load saved theme
 const savedTheme = localStorage.getItem("theme");
 if (savedTheme) {
-  stylesheet.href = savedTheme;
+    stylesheet.href = savedTheme;
+} else {
+    stylesheet.href = DARK;
 }
- 
-toggleButton.addEventListener("click", () => {
-  const newTheme = stylesheet.href.includes(LIGHT)
-    ? DARK
-    : LIGHT;
-  stylesheet.href = newTheme;
-  localStorage.setItem("theme", newTheme);
-});
+
+function toggleTheme() {
+
+    const newTheme = stylesheet.href.includes(LIGHT)
+        ? DARK
+        : LIGHT;
+    stylesheet.href = newTheme;
+    localStorage.setItem("theme", newTheme);
+}
+
+toggleButton.addEventListener("click", toggleTheme);
