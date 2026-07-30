0d0a8c65f4af5b8b9109be228e5fe5d53abfb6e1
andrei-lazer <andrei.lazer319@gmail.com>
2026-07-30 23:07:39 +0100

    Deploy: 30/07/2026

--- src/pages/now.lisp
+++
@@ -1,8 +0,0 @@
-(defpackage #:pages/now
-  (:use #:cl)
-  (:export #:render))
-
-(defun pages/now:render ()
-  (layouts/main:html5
-    (:title "now" :icon-file "jellyfish.gif")
-    (:p "This is my now page!")))
